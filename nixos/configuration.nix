a90851641d74182bf99d6e8d67b066aa47dbb833
lsdhophora <lsdphophora@proton.me>
2025-08-16 00:07:52 +0800

    Update(25/8/16)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -114,7 +114,7 @@
     '';
   };
 
-  programs.firefox.enable = true;
+  # programs.firefox.enable = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
@@ -148,7 +148,7 @@
   fonts = {
     enableDefaultPackages = true;
     packages = with pkgs; [
-      noto-fonts-cjk-sans
+      noto-fonts-cjk-sans-static
       noto-fonts-cjk-serif-static
       noto-fonts
       noto-fonts-extra
--- nixos/home.nix
+++ nixos/home.nix
@@ -1,12 +1,21 @@
 { config, pkgs, ... }:
-
+let
+  # 要删除的词列表
+  deleteWords = [ ];
+in
 {
   home.username = "lophophora";
   home.homeDirectory = "/home/lophophora";
   home.stateVersion = "25.11";
 
-  # Clone rime-ice from GitHub
-  home.file.".config/ibus/rime" = {
+  # 安装必要包
+  home.packages = with pkgs; [
+    gnomeExtensions.paperwm
+    gnomeExtensions.hide-universal-access
+  ];
+
+  # Rime 配置
+  xdg.configFile."ibus/rime" = {
     source = pkgs.fetchFromGitHub {
       owner = "iDvel";
       repo = "rime-ice";
@@ -14,10 +23,45 @@
       sha256 = "sha256-nPkwX5ruvCkQZ4yKNVuh72RmbLiEs/lkjb7mPQURVWw=";
     };
     recursive = true;
+    force = true; # 允许覆盖现有文件
+    # 在写入后保留 rime_ice.userdb 并删除指定词
+    onChange = ''
+      RIME_DIR="$HOME/.config/ibus/rime"
+      # 备份 rime_ice.userdb 和词库文件
+      if [ -f "$RIME_DIR/rime_ice.userdb" ]; then
+        cp -v "$RIME_DIR/rime_ice.userdb" "$RIME_DIR/rime_ice.userdb.bak"
+      fi
+      # 备份词库文件以保留用户修改
+      for file in "$RIME_DIR/cn_dicts"/*.dict.yaml "$RIME_DIR/en_dicts"/*.dict.yaml "$RIME_DIR/opencc/other.txt"; do
+        if [ -f "$file" ]; then
+          cp -v "$file" "$file.bak"
+        fi
+      done
+      # 删除指定词
+      ${pkgs.lib.concatMapStringsSep "\n" (word: ''
+        for file in "$RIME_DIR/cn_dicts"/*.dict.yaml "$RIME_DIR/en_dicts"/*.dict.yaml "$RIME_DIR/opencc/other.txt"; do
+          if [ -f "$file.bak" ]; then
+            echo "Removing entries containing '${word}' from $file"
+            ${pkgs.gnugrep}/bin/grep -vF "${word}" "$file.bak" > "$file"
+          fi
+        done
+      '') deleteWords}
+      # 恢复 rime_ice.userdb
+      if [ -f "$RIME_DIR/rime_ice.userdb.bak" ]; then
+        mv -v "$RIME_DIR/rime_ice.userdb.bak" "$RIME_DIR/rime_ice.userdb"
+      fi
+      # 清理备份的词库文件
+      for file in "$RIME_DIR/cn_dicts"/*.dict.yaml.bak "$RIME_DIR/en_dicts"/*.dict.yaml.bak "$RIME_DIR/opencc/other.txt.bak"; do
+        if [ -f "$file" ]; then
+          rm -v "$file"
+        fi
+      done
+      echo "Rime directory contents after operations:"
+      ls -l "$RIME_DIR"
+    '';
   };
 
-  # Create rime_ice.custom.yaml to patch editor.bindings
-  home.file.".config/ibus/rime/rime_ice.custom.yaml".text = ''
+  xdg.configFile."ibus/rime/rime_ice.custom.yaml".text = ''
     patch:
       editor/bindings:
         space: commit_raw_input
@@ -31,8 +75,7 @@
         Escape: cancel
   '';
 
-  # Configure default.custom.yaml, removing schema_list
-  home.file.".config/ibus/rime/default.custom.yaml".text = ''
+  xdg.configFile."ibus/rime/default.custom.yaml".text = ''
     patch:
       ascii_composer/good_old_caps_lock: true
       ascii_composer/switch_key:
@@ -42,55 +85,52 @@
         Control_L: inline
         Control_R: noop
       key_binder/bindings:
-        # Emacs style key bindings
-        - { when: composing, accept: Control+p, send: Up }          # Previous candidate
-        - { when: composing, accept: Control+n, send: Down }        # Next candidate
-        - { when: composing, accept: Control+f, send: Right }       # Move cursor forward
-        - { when: composing, accept: Control+b, send: Left }        # Move cursor backward
-        - { when: composing, accept: Control+a, send: Home }        # Move to start of input
-        - { when: composing, accept: Control+e, send: End }         # Move to end of input
-        - { when: composing, accept: Control+d, send: Delete }      # Delete character after cursor
-        - { when: composing, accept: BackSpace, send: BackSpace }   # Delete character before cursor
-        - { when: composing, accept: Control+k, send: delete_to_end }  # Delete to end
+        - { when: composing, accept: Control+p, send: Up }
+        - { when: composing, accept: Control+n, send: Down }
+        - { when: composing, accept: Control+f, send: Right }
+        - { when: composing, accept: Control+b, send: Left }
+        - { when: composing, accept: Control+a, send: Home }
+        - { when: composing, accept: Control+e, send: End }
+        - { when: composing, accept: Control+d, send: Delete }
+        - { when: composing, accept: BackSpace, send: BackSpace }
+        - { when: composing, accept: Control+k, send: delete_to_end }
   '';
 
-  # Override ibus_rime.yaml
-  home.file.".config/ibus/rime/ibus_rime.yaml" = {
-    text = ''
-      __build_info:
-        rime_version: ${pkgs.librime.version}
-        timestamps:
-          ibus_rime: 0
-          ibus_rime.custom: 0
+  xdg.configFile."ibus/rime/ibus_rime.yaml".text = ''
+    __build_info:
+      rime_version: ${pkgs.librime.version}
+      timestamps:
+        ibus_rime: 0
+        ibus_rime.custom: 0
       config_version: 1.0
       style:
         cursor_type: insert
         horizontal: true
         inline_preedit: true
         preedit_style: preview
-    '';
-  };
-
-  # Add activation script to clean .config/ibus/rime except rime_ice.userdb and ensure only one ibus_rime.yaml
-  home.activation.cleanRimeConfig = config.lib.dag.entryBefore [ "writeBoundary" ] ''
-    if [ -d "$HOME/.config/ibus/rime" ]; then
-      # Remove all ibus_rime.yaml files first
-      ${pkgs.findutils}/bin/find "$HOME/.config/ibus/rime" -maxdepth 1 -name 'ibus_rime.yaml' -exec rm -f {} +
-      # Remove other files except rime_ice.userdb
-      ${pkgs.findutils}/bin/find "$HOME/.config/ibus/rime" -maxdepth 1 -not -name 'rime_ice.userdb' -not -name 'ibus_rime.yaml' -exec rm -rf {} +
-    fi
   '';
 
-  home.packages = with pkgs; [
-    gnomeExtensions.paperwm # 如果存在于 Nixpkgs
-  ];
-
-  # 启用 Gnome 扩展
+  # GNOME 设置
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [
-        "paperwm@paperwm.github.com" # 替换为 PaperWM 的扩展 UUID
+        "paperwm@paperwm.github.com"
+        "hide-universal-access@akiirui.github.io"
       ];
+    };
+    "org/gnome/desktop/interface" = {
+      font-name = "Adwaita Sans 12";
+      text-scaling-factor = 1.04; # 缩放因子
+    };
+  };
+
+  # Firefox 配置
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      settings = {
+        "layout.css.devPixelsPerPx" = "1.1";
+      };
     };
   };
 }
